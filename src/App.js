b4d998a1c81c3bf7ddca614f6509e38468a3373e
yejinGim <yjgim@wesome.co.kr>
2022-10-11 17:54:21 +0900

    reducer로 로직 나누기(init과 create까지 진행)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-//https://jsonplaceholder.typicode.com/comments
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data
+    }
+    case "CREATE": {
+      const create_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        create_date,
+      }
+      return [newItem, ...state];
+    }
+    case "REMOVE": {
+
+    }
+    case "EDIT": {
+
+    }
+    default: {
+
+    }
+      return state;
+  }
+}
 function App() {
-  const [data, setData] = useState([]);
+  const [data, dispatch] = useReducer(reducer, []);
   const dataId = useRef(0);
 
   //api를 이용해서 적절히 데이터를 가공해서 초기 값을 설정해봄
@@ -14,44 +38,50 @@ function App() {
     //원하는 json값들만 출력
     const res = await fetch('https://jsonplaceholder.typicode.com/comments'
     ).then((res) => res.json());
+
+
     const initData = res.slice(0, 20).map((it) => {
       return {
         author: it.email,
         content: it.body,
         emotion: Math.floor(Math.random() * 5) + 1,
         created_date: new Date().getTime(),
-        id: dataId.current++
+        dataId: dataId.current++
       }
     })
-    setData(initData);
+    dispatch({ type: "INIT", data: initData })
   };
+
 
   useEffect(() => {
     getData();
   }, [])
   const onCreate = (author, content, emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      dataId: dataId.current
-    };
+
+    dispatch({
+      type: "CREATE",
+      data: {
+        author,
+        content,
+        emotion,
+        dataId: dataId.current
+      }
+    })
     dataId.current++;
-    setData([newItem, ...data]);
   }
 
   const onRemove = (targetId) => {
     const newDiaryList = data.filter((data) => data.dataId !== targetId);
-    setData(newDiaryList);
+    dispatch({ type: "REMOVE", data: newDiaryList })
   }
 
   const onEdit = (targetId, newContent) => {
-    setData(
-      data.map(it => it.dataId === targetId ? { ...it, content: newContent } : it
+    dispatch({
+      type: "EDIT",
+      data: data.map(it => it.dataId === targetId ? { ...it, content: newContent } : it
       )
-    );
+    })
+
   };
   return (
     <div className='App'>
